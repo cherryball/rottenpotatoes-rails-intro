1d976af80caf71dcdb7384dd61f9976e28cbc0da
cherryball <greencherryball@gmail.com>
2015-11-14 06:21:53 +0000

    Add hilite on sorted column header

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
 
+  attr_accessor :styles
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date, :sort_by_column)
   end
@@ -11,12 +12,19 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @styles = { 'title_header' => '', 'release_date_header' => '' }
     if params[:sort_by_column].nil?
         @movies = Movie.all
     elsif params[:sort_by_column] == 'title'
       @movies = Movie.order('title asc')
+      @styles['title_header'] = 'hilite'
+      @styles['release_date_header'] = ''
+      flash[:notice] = "Sort by Title"
     elsif params[:sort_by_column] == 'release_date'
       @movies = Movie.order('release_date asc')
+      @styles['release_date_header'] = 'hilite'
+      @styles['title_header'] = ''
+      flash[:notice] = "Sort by Release Date"
     end
   end
 
